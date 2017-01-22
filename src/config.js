ad581d4797ab43e429ddb4fe0f241933be50ffbd
LechuckRoh <member@lechuck2011.cafe24.com>
2017-01-22 16:45:22 +0900

    Create config file on startup if not exist

--- src/config.js
+++ src/config.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const fs = require('fs-extra');
+const path = require('path');
+
+const cfgPath = path.join(__dirname, '../config/config.json');
+
+// Create config file if not exists.
+if (!fs.existsSync(cfgPath)) {
+    const sampleCfgPath = path.join(__dirname, '../config/config.sample.json');
+    fs.copySync(sampleCfgPath, cfgPath);
+
+    // Exit process with return code = 1
+    console.log(`Configuration file created : ${cfgPath}`);
+    console.log('Edit configuration file and start again.');
+    process.exit(1);
+}
+
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(cfgPath)[env];
 
 module.exports = config;
